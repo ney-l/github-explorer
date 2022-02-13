6db20e8358549eeded9ef298503e275d7dec4e4a
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-13 19:51:30 +0530

    Updates file name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from 'pages/Home';
 import { About } from 'pages/About';
-import { User } from 'pages/Users';
+import { User } from 'pages/User';
 import { NotFound } from 'pages/NotFound';
 import { Navbar } from 'components/layout/Navbar';
 import { Footer } from 'components/layout/Footer';
