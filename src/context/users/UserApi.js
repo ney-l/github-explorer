111f18cc421a3b14599598053e868b7c4c95af4b
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-14 21:38:23 +0530

    configures defaults for api calls

--- src/context/users/UserApi.js
+++ src/context/users/UserApi.js
@@ -3,19 +3,15 @@ import axios from 'axios';
 const API_URL = process?.env?.REACT_APP_API_URL;
 const API_TOKEN = process?.env?.REACT_APP_API_TOKEN;
 
+axios.defaults.baseURL = `https://${API_URL}`;
+axios.defaults.headers.common['Authorization'] = `token ${API_TOKEN}`;
+
 export async function fetchUsers(text) {
   try {
     const params = new URLSearchParams({
       q: text,
     });
-    const { data } = await axios.get(
-      `https://${API_URL}/search/users?${params}`,
-      {
-        headers: {
-          Authorization: `token ${API_TOKEN}`,
-        },
-      }
-    );
+    const { data } = await axios.get(`/search/users?${params}`);
     const { items: users } = data;
     return { users };
   } catch (err) {
@@ -26,11 +22,7 @@ export async function fetchUsers(text) {
 
 export async function getUser(username) {
   try {
-    const { data } = await axios.get(`https://${API_URL}/users/${username}`, {
-      headers: {
-        Authorization: `token ${API_TOKEN}`,
-      },
-    });
+    const { data } = await axios.get(`/users/${username}`);
     return { user: data };
   } catch (err) {
     const { status, data } = err.response ?? {};
