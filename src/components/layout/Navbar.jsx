87597da633e2310f2210bda253ae783aa39f2ea0
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-15 20:19:19 +0530

    Updates logo

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,13 +1,19 @@
 import { FaGithub } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import logo from 'logo.svg';
 
 export function Navbar({ title }) {
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-          <FaGithub className="inline pr-2 text-3xl" />
+          <img
+            src={logo}
+            alt="Logo"
+            width={50}
+            className="inline pr-2 text-3xl"
+          />
           <Link to="/" className="text-lg font-bold align-middle">
             {title}
           </Link>
