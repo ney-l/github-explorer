48bd6afa06c36105a819ad3f0e2f569bfa7c8275
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-16 16:42:23 +0530

    Adds alt text for image

--- src/components/users/ProfileImage.jsx
+++ src/components/users/ProfileImage.jsx
@@ -3,7 +3,7 @@ export function ProfileImage({ name, avatarUrl }) {
     <div className="custom-card-image mb-6 md:mb-0">
       <div className="rounded-lg shadow-xl card image-full">
         <figure>
-          <img src={avatarUrl} alt="" />
+          <img src={avatarUrl} alt="User Profile" />
         </figure>
         <div className="card-body justify-end">
           <h2 className="card-title mb-0">{name}</h2>
