eee0dfb5e7cee623bf23f1ce4777fee51d0ccca5
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-16 11:49:47 +0530

    adds repo link to about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,7 @@
+import { FaLink } from 'react-icons/fa';
+
 export function About() {
+  const repoLink = 'https://github.com/ney-l/github-explorer';
   return (
     <div>
       <h1 className="text-6xl mb-4">Github Explorer</h1>
@@ -14,10 +17,22 @@ export function About() {
         Version: <span className="text-white">1.0.0</span>
       </p>
       <p className="text-lg text-gray-400">
-        Technologies Used:
+        Technologies Used:{' '}
         <span className="text-white">
           React, React Router Dom, Tailwind CSS, Daisy UI, GitHub REST API.
         </span>
+      </p>
+      <p className="text-lg text-gray-400">
+        Code Repository:{' '}
+        <a
+          target="_blank"
+          className="text-white"
+          rel="noreferrer"
+          href={repoLink}
+        >
+          <FaLink className="inline mr-1" />
+          {repoLink}
+        </a>
       </p>
     </div>
   );
