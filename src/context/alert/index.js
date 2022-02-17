b6692d855ea7261fa044b43688f0d8ca939910bf
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-17 16:23:22 +0530

    fixes reference before init error

--- src/context/alert/index.js
+++ src/context/alert/index.js
@@ -1,5 +1,4 @@
 export * from 'context/alert/AlertActions';
 export * from 'context/alert/AlertActionTypes';
-export * from 'context/alert/AlertContext';
 export * from 'context/alert/AlertHook';
 export * from 'context/alert/AlertReducer';
