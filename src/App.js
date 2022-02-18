86e49124b29c282b440930b02618e924e67a1426
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-18 17:38:37 +0530

    For enabling testing main app

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import { Home, About, User, NotFound } from 'pages';
 import { Alert, Footer, Navbar } from 'components/layout';
 import { AlertProvider, UserProvider } from 'context';
 
-function App() {
+export function App() {
+  return (
+    <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+      <main className="container mx-auto px-3 pb-12">
+        <Alert />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users/:username" element={<User />} />
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export function Root() {
   return (
     <UserProvider>
       <AlertProvider>
         <Router basename="github-explorer">
-          <div className="flex flex-col justify-between h-screen">
-            <Navbar />
-            <main className="container mx-auto px-3 pb-12">
-              <Alert />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/users/:username" element={<User />} />
-                <Route path="/notfound" element={<NotFound />} />
-                <Route path="/*" element={<NotFound />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
+          <App />
         </Router>
       </AlertProvider>
     </UserProvider>
