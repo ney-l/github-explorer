5abaf8e997c6deab218a71ae75a1085c6957c51a
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-18 14:43:40 +0530

    refactors to use ternary

--- src/context/users/UserHooks.js
+++ src/context/users/UserHooks.js
@@ -2,7 +2,7 @@ import { useReducer } from 'react';
 import { UserActions, UserRepoActions, UserSearchActions } from './UserActions';
 import { Api, userReducer } from 'context/users';
 
-const initialState = {
+export const initialState = {
   users: [],
   user: {},
   isLoading: false,
@@ -16,32 +16,27 @@ export function useSearchUsers() {
   async function searchUsers(text) {
     dispatch(UserSearchActions.startLoading());
     const { users, error } = await Api.fetchUsers(text);
-    if (error) {
-      return dispatch(UserSearchActions.error(error));
-    }
-    dispatch(UserSearchActions.success(users));
+    return error
+      ? dispatch(UserSearchActions.error(error))
+      : dispatch(UserSearchActions.success(users));
   }
 
-  async function clearUsers() {
-    dispatch(UserSearchActions.clear());
-  }
+  const clearUsers = () => dispatch(UserSearchActions.clear());
 
   async function getUser(username) {
     dispatch(UserActions.startLoading());
     const { user, error } = await Api.getUser(username);
-    if (error) {
-      return dispatch(UserActions.error(error));
-    }
-    dispatch(UserActions.success(user));
+    return error
+      ? dispatch(UserActions.error(error))
+      : dispatch(UserActions.success(user));
   }
 
   async function getUserRepos(username) {
     dispatch(UserRepoActions.startLoading());
     const { repos, error } = await Api.getRepos(username);
-    if (error) {
-      return dispatch(UserRepoActions.error());
-    }
-    dispatch(UserRepoActions.success(repos));
+    return error
+      ? dispatch(UserRepoActions.error(error))
+      : dispatch(UserRepoActions.success(repos));
   }
 
   return { state, searchUsers, clearUsers, getUser, getUserRepos };
