3b59a74d0f7e9311833aa485f8a0c083a68dea10
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-18 19:31:35 +0530

    makes logo clickable

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -9,13 +9,13 @@ export function Navbar({ title }) {
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-          <img
-            src={logo}
-            alt="Logo"
-            width={50}
-            className="inline pr-2 text-3xl"
-          />
           <Link to="/" className="text-lg font-bold align-middle">
+            <img
+              src={logo}
+              alt="Logo"
+              width={50}
+              className="inline pr-2 text-3xl"
+            />
             {title}
           </Link>
         </div>
